ff313c93c629e9358bf09f65dc6503a73dfadf04
面向对象 <44747536+520MianXiangDuiXiang520@users.noreply.github.com>
2018-12-12 22:19:28 +0800

    Update 0000.py

--- pythonday/0000.py
+++ pythonday/0000.py
@@ -6,37 +6,37 @@ from PIL import Image, ImageDraw, ImageFont
 im=Image.open('0000/0000.jpg','r')
 #打开图片
 
-#im.show()
+im.show()
 
 #保存图片
 
-#im.save("0000/0000.jpeg",'jpeg')
+im.save("0000/0000.jpeg",'jpeg')
 
 #裁剪图片,xy为裁剪的坐标
 
-#xy=(10,10,100,100)
-#imcut=im.crop(xy)
-#imcut.show()
-#imcut.save("0000/cut.jpeg",'jpeg')
+xy=(10,10,100,100)
+imcut=im.crop(xy)
+imcut.show()
+imcut.save("0000/cut.jpeg",'jpeg')
 
 #图片拼合
 #把imc拼接到 backgroundimc 的（200，200）处
 
-#backgroundimc=im
-#imc=Image.open('0000/cut.jpeg')
-#backgroundimc.paste(imc,(200,200))
-#backgroundimc.show()
-#backgroundimc.save('0000/paste.jpeg','jpeg')
+backgroundimc=im
+imc=Image.open('0000/cut.jpeg')
+backgroundimc.paste(imc,(200,200))
+backgroundimc.show()
+backgroundimc.save('0000/paste.jpeg','jpeg')
 
 #图片缩放
 
-#(x,y)=im.size
-#change_x=1200
+(x,y)=im.size
+change_x=1200
 #注意，//！！change_x,change_y参数需要时整数，不能用“/”。要用“//”
-#change_y=y*change_x//x
-#out=im.resize((change_x,change_y),Image.ANTIALIAS)
-#out.show()
-#out.save('0000/resize.jpeg','jpeg')
+change_y=y*change_x//x
+out=im.resize((change_x,change_y),Image.ANTIALIAS)
+out.show()
+out.save('0000/resize.jpeg','jpeg')
 
 #在图片上写字
 #imagefont参考csdn，https://blog.csdn.net/icamera0/article/details/50762050
